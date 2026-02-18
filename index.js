1fcefb9de90e40df3e17ea8c4568b61d22ad9439
Luis <luisffcc475@gmail.com>
2026-02-17 22:08:32 -0300

    Projeto concluido preparado para deploy no vercel

--- index.js
+++ index.js
@@ -44,6 +44,59 @@ server.get('/horaAtual', (requisicao,resposta) =>
     )
 })
 
+//criar um metodo que aceita parametros
+server.get('/tabuada', (requisicao,resposta)=> {
+    const numero = requisicao.query.numero;
+    const sequencia = requisicao.query.sequencia;
+    if(!numero || !sequencia)
+    {
+        resposta.send(`
+              <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="assets/css/styl.css">
+                    <title>Document</title>
+                </head>
+                <body>
+                <h1> Tabuada</h1>
+                <h2>Olám seja bem vindo a pagina inicial</h2>
+                <h3>Por informe o numero e a sequencia na url</h3>
+                <h4>Exemplo: http://localhost:3000/tabuada?numero=9&sequencia=100</h4>
+                </body>
+             </html>
+            `)
+    }else{
+        resposta.write(`  <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="assets/css/styl.css">
+                    <title>Document</title>
+                </head>
+                <body>
+                <h1> Tabuada do ${numero} até a sequencia ${sequencia}</h1>
+                <ul>
+                `);
+                for (let i=0; i<=sequencia;i++)
+                {
+                    resposta.write(`<li>${i} x ${numero} = ${i * numero}</li>`);
+                }
+                resposta.write(`
+                        </ul>
+                    </body>
+                    </html>
+                    `)
+        resposta.end(); //finaliza e envia
+    }
+});
+
+
+
+
+
 server.listen(porta, host, ()=>{
 console.log(`Servidor escutando em http://${host}:${porta}`);
 
